0a8ce9827986862167d025ac363aa00996f5c904
Joseph Lenton <JLenton@ipipeline.com>
2024-01-23 10:20:47 +0000

    first commit

---
+++ Crispy Goggles/Crispy Goggles/Controllers/SecurityController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Crispy_Goggles.Controllers
+{
+    public class SecurityController : Controller
+    {
+        private readonly ILogger<SecurityController> _logger;
+
+        public SecurityController(ILogger<SecurityController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ Crispy Goggles/Crispy Goggles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Crispy_Goggles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
