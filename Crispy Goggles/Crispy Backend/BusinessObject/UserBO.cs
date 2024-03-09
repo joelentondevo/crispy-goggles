7cb51a0b769ae345e0a59a2e1b6e9a734623df8e
joelentondevo <jlenton@me.com>
2024-03-09 23:29:49 +0000

    crispy backend structure

---
+++ Crispy Goggles/Crispy Backend/BusinessObject/UserBO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crispy_Backend.BusinessObject
+{
+    // contains the methods to interact with UserDO, retrieving UserEO etc
+    internal class UserBO
+    {
+    }
+}
---
+++ Crispy Goggles/Crispy Backend/DataObjects/BaseDO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crispy_Backend.DataObjects
+{
+    internal class BaseDO()
+    {
+
+    }
+}
