0eb6fdc36132a52f7f5f31f07c501d9e30a41529
joelentondevo <jlenton@me.com>
2024-04-04 14:44:23 +0100

    basket groundwork

---
+++ Crispy Goggles/Crispy Backend/EntityObjects/BasketEO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crispy_Backend.EntityObjects
+{
+    public class BasketEO
+    {
+        public required List<ItemEO> Items { get; set; }
+
+        public int CalculateTotal()
+        {
+            int total = 0;
+            foreach (ItemEO item in Items)
+            {
+                total += item.Price;
+            };
+            return total;
+        }
+
+        public int CountItem(string itemtocount)
+        {
+            int count = 0;
+            foreach (ItemEO item in Items) 
+            {
+                if (item.Name == itemtocount)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Crispy Goggles/Crispy Backend/EntityObjects/ItemEO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crispy_Backend.EntityObjects
+{
+    public class ItemEO
+    {
+        public int Price { get; set; }
+        public string Name { get; set; }
+    }
+}
